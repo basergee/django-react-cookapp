2233a074354cb71456be1482d85d7687db4e21d8
Sergey Bachurin <ba4urin.sergey2016@yandex.ru>
2022-12-11 00:45:57 +0500

    Настроил Django на выдачу титульной страницы react-приложения

--- cookapp/frontend/webpack.config.js
+++ cookapp/frontend/webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: path.join(__dirname, "/dist"),
-        filename: 'main.js'
+        filename: 'static/main.js'
     },
 
     /* Настройки webpack-dev-server */
