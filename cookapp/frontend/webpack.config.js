37e0469ff7bb4d9a4c52f1ab4a0969d3419193ff
Sergey Bachurin <ba4urin.sergey2016@yandex.ru>
2022-12-10 23:00:01 +0500

    Добавил заготовку фронтенда на React

---
+++ cookapp/frontend/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require ('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+//const Dotenv = require('dotenv-webpack');
+
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, "/dist"),
+        filename: 'main.js'
+    },
+
+    /* Настройки webpack-dev-server */
+    devServer: {
+        static: './dist',
+    },
+
+    plugins:[
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        })
+        // new Dotenv()
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: '/node_modules',
+                loader: 'babel-loader'
+            }
+        ]
+    }
+};
