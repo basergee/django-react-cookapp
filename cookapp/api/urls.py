5b577ca677de1ceb33998ed8b05f6ecb64887693
Sergey Bachurin <ba4urin.sergey2016@yandex.ru>
2022-12-12 21:13:49 +0500

    Добавил эндпоинты для вывод всех рецептов выбраной категории

--- cookapp/api/urls.py
+++ cookapp/api/urls.py
@@ -4,4 +4,5 @@ from .views import CategoryViewSet
 
 urlpatterns = [
     path('dishes/', CategoryViewSet.as_view({'get': 'list'})),
+    path('dishes/<int:pk>/', CategoryViewSet.as_view({'get': 'retrieve'})),
 ]
--- cookapp/api/views.py
+++ cookapp/api/views.py
@@ -1,9 +1,22 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 
-from .models import Category
-from .serializers import CategorySerializer
+
+from .models import Category, Recipe
+from .serializers import CategorySerializer, RecipeSerializer
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+    def retrieve(self, request, pk=None):
+        if Category.objects.filter(pk=pk).exists():
+            queryset = Recipe.objects.filter(
+                category=Category.objects.get(pk=pk)
+            )
+            serializer = RecipeSerializer(queryset, many=True)
+            return Response(serializer.data)
+        else:
+            content = {'Нет категории': 'Выберите другую категорию'}
+            return Response(content, status=status.HTTP_404_NOT_FOUND)
