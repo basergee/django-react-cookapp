976e74bb3b9acba28bb819fa52d626544752ce47
Sergey Bachurin <ba4urin.sergey2016@yandex.ru>
2022-12-18 23:48:25 +0500

    Превратил названия рецептов в ссылки и сделал обработку переходов по ним

--- cookapp/frontend/src/pages/DishCategory.js
+++ cookapp/frontend/src/pages/DishCategory.js
@@ -1,10 +1,36 @@
 import React, {useState, useEffect} from 'react'
 
+import axios from 'axios';
+import { Link, useParams } from 'react-router-dom';
+
 
 const DishCategory = () => {
+    let [recipes, setRecipesList] = useState([]);
+    let id = useParams().id;
+
+    useEffect(() => {
+        const url = `/api/dishes/` + id;
+        axios.get(url)
+            .then(res => {
+                setRecipesList(res.data);
+            })
+            .catch(error => {
+                console.log(error.response.statusText);
+            });
+    }, []);
+
     return(
         <div>
             <h3>List of recipes in this category</h3>
+            <div className="recipes-list">
+                <ul>
+                    {recipes.map( (recipe, index) =>
+                        (<li key={index}>
+                            <Link to={'recipes/' + recipe.id}>{recipe.title}</Link>
+                        </li>) )
+                    }
+                </ul>
+            </div>
         </div>
     );
 }
