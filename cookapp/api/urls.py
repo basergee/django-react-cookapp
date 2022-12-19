5eecebc438d6f29c19da7b3162b878d20319074b
Sergey Bachurin <ba4urin.sergey2016@yandex.ru>
2022-12-19 22:04:39 +0500

    Вывел страницу документации к API с помощью swagger-ui

--- cookapp/api/urls.py
+++ cookapp/api/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.views.generic import TemplateView
+
 from .views import CategoryViewSet, RecipeViewSet
 
 
@@ -8,4 +10,10 @@ urlpatterns = [
 
     path('recipes/', RecipeViewSet.as_view({'get': 'list'})),
     path('recipes/<int:pk>/', RecipeViewSet.as_view({'get': 'retrieve'})),
+
+    # Выводим страницу с документацией к API
+    path('', TemplateView.as_view(
+        template_name='swagger-ui.html',
+        extra_context={'schema_url': 'openapi-schema.yml'}
+    ), name='swagger-ui'),
 ]
